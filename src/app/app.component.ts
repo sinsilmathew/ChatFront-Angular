bc20d624eeed94d14867d09885e78a2ef90f6090
sinsil <sincilmathew@gmail.com>
2018-07-06 15:27:39 +0530

    "FRontEnd"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 //component decorator
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',//template:'<h1>Hello</h1>',
   styleUrls: ['./app.component.css']
 })
 
 //all logical parts
 export class AppComponent {
-  title = 'app';
+  title:string = 'Katerra Construct';
 }
